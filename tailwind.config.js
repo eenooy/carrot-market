404b4da35184df628227a582f0288392d2a3c737
박소윤 <eenooyos@mncnext.co.kr>
2022-09-21 15:46:43 +0900

    Add: 로그인 폼 레이아웃

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}", 
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  darkMode: "media",
+  plugins: [require("@tailwindcss/forms")],
+};
