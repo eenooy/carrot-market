6c0c0a13d7c6eb01633f62180757078ce0b7559d
eenooyos <eenooyos@gmail.com>
2022-12-02 09:45:21 +0900

    modify: 줄정리

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -29,7 +29,6 @@ const CommunityPostDetail: NextPage = () => {
                             viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
-                            
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
