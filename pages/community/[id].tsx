97883a2c1d3dc1d084cb2dd8bff353541fdb0549
eenooyos <eenooyos@gmail.com>
2022-12-06 13:09:16 +0900

    modify: 줄바꿈

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -46,6 +46,7 @@ const CommunityPostDetail: NextPage = () => {
                             viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                         >
+
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
