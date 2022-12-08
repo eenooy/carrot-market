3bbc6df423d1ccbd97f2a5b72f1db905d4046cbe
eenooyos <eenooyos@gmail.com>
2022-12-08 16:57:44 +0900

    modify: 줄바꿈

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -8,8 +8,7 @@ const Community: NextPage = () => {
                 {[1, 2, 3, 4, 5, 6].map((_, i) => (
                     <div
                         key={i}
-                        className="flex cursor-pointer flex-col pt-4 items-start"
-                    >
+                        className="flex cursor-pointer flex-col pt-4 items-start">
                         <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
                             동네질문
                         </span>
