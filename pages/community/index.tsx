55687e3d6aa4593db805223ae243f136ecb32cc8
eenooyos <eenooyos@gmail.com>
2022-12-09 09:53:03 +0900

    Modify: 줄바꿈

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -13,8 +13,8 @@ const Community: NextPage = () => {
                             동네질문
                         </span>
                         <div className="mt-2 px-4 text-gray-700">
-                            <span className="text-orange-500 font-medium">Q.</span> What is
-                            the best mandu restaurant?
+                            <span className="text-orange-500 font-medium">Q.</span>
+                            What is the best mandu restaurant?
                         </div>
                         <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
                             <span>니꼬</span>
